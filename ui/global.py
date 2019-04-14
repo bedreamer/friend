9ac6c379bf1dafdb4d3ea0f13dc960f520195e26
tom <bedreamer@163.com>
2019-04-14 21:41:32 +0800

    添加am335x的串口接口选择

--- ui/global.py
+++ ui/global.py
@@ -1,10 +1,20 @@
 import codecs
 import os
 
+uname = os.uname()
+
+system_name = uname[0]
+host_name = uname[1]
+os_version = uname[2]
+
 
 def global_var(request):
     content = dict()
     serial_number_file = os.path.dirname(__file__) + '/device-serial-number.txt'
+
+    content['system'] = system_name
+    content['host_name'] = host_name
+    content['os_version'] = os_version
 
     try:
         with codecs.open(serial_number_file, encoding="utf8") as file:
