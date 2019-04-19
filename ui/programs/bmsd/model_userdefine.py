22bc8f41c72b6d516a0ae3dad1740b9e1b42e89f
bedreamer <bedreamer@163.com>
2019-04-19 08:56:58 +0800

    完成自定义CAN报文的编码

--- ui/programs/bmsd/model_userdefine.py
+++ ui/programs/bmsd/model_userdefine.py
@@ -13,34 +13,40 @@ bms_data = dict()
 
 
 def get_bms_definition(bms_model):
-    url = ''.join(['http://' ,settings.web_host, ':', str(settings.web_port),
-                   '/bms/query/', '?bms_name=', bms_model])
+    url = ''.join(['http://', settings.web_host, ':', str(settings.web_port), '/bms/query/', '?bms_name=', bms_model])
     f = urllib.request.urlopen(url)
     resolution_txt = f.read().decode('utf-8')
-
     return json.loads(resolution_txt)
 
 
 bms_define = get_bms_definition(settings.bms_model)
-
-
-def on_BMS_TempInfo(bits64):
-    pack = dict()
-
-    pack['TempMin'] = int(bits64[32:40][::-1], 2) - 40
-    pack['TempMax'] = int(bits64[40:48][::-1], 2) - 40
-    pack['TempAver'] = int(bits64[48:56][::-1], 2) - 40
-    logging.debug(pack)
-
-    global bms_data
-    bms_data = dict(bms_data, **pack)
-    r = redis.Redis(connection_pool=settings.redis_pool)
-    r.set(bms_data_cache_path, json.dumps(bms_data, ensure_ascii=False, indent=2), ex=5)
-
-
-filter_address = {
-    0x364: on_BMS_TempInfo,
+bms_fid_set = {
+    bms_define['avr']['fid'],
+    bms_define['max']['fid'],
+    bms_define['min']['fid'],
 }
+
+
+def _parse_field_in_64bits(field, name, bits64):
+    bb = field['sb']
+    eb = bb + field['bl']
+    x = field['x']
+    offset = field['offset']
+
+    value = int(bits64[bb:eb][::-1], 2) * x + offset
+    return {name: value}
+
+
+def on_parse_bms_avg_temp(bits64):
+    return _parse_field_in_64bits(bms_define['avr'], 'TempAver', bits64)
+
+
+def on_parse_bms_min_temp(bits64):
+    return _parse_field_in_64bits(bms_define['min'], 'TempMin', bits64)
+
+
+def on_parse_bms_max_temp(bits64):
+    return _parse_field_in_64bits(bms_define['max'], 'TempMax', bits64)
 
 
 def get_filter_id_range_pairs_list():
@@ -48,11 +54,12 @@ def get_filter_id_range_pairs_list():
     max_id = bms_define['max']['fid']
     min_id = bms_define['min']['fid']
 
-    return [(x, x) for x in {avr_id, max_id, min_id}]
+    id_ranges_list = [(x, x) for x in {avr_id, max_id, min_id}]
+    return id_ranges_list
 
 
 def on_frame(fid, bytes_data):
-    if fid not in filter_address:
+    if fid not in bms_fid_set:
         return
 
     if len(bytes_data) < 8:
@@ -61,5 +68,22 @@ def on_frame(fid, bytes_data):
     interger = struct.unpack('<Q', bytes_data)[0]
     _bits64 = bin(interger)[2:]
     bits64 = '0' * (64 - len(_bits64)) + _bits64
-    filter_address[fid](bits64[::-1])
 
+    package = dict()
+
+    if fid == bms_define['avr']['fid']:
+        pack = on_parse_bms_avg_temp(bits64)
+        dict(package, **pack)
+
+    if fid == bms_define['avr']['max']:
+        pack = on_parse_bms_max_temp(bits64)
+        dict(package, **pack)
+
+    if fid == bms_define['avr']['min']:
+        pack = on_parse_bms_min_temp(bits64)
+        dict(package, **pack)
+
+    global bms_data
+    bms_data = dict(bms_data, **package)
+    r = redis.Redis(connection_pool=settings.redis_pool)
+    r.set(bms_data_cache_path, json.dumps(bms_data, ensure_ascii=False, indent=2), ex=5)
